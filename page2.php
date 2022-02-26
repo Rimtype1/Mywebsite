7bfdcf185b8b6b3638e99a0b939c3056dfd12907
Rimtype1 <100473568+Rimtype1@users.noreply.github.com>
2022-02-26 19:32:33 +0700

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			#box2{
+				width: 600px;
+				height: 150px;
+			}
+			.box{
+				background-color: green;
+			}
+			body{
+				background-color: grey;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="box1" class="box">
+			<h1>กล่องที่ 1</h1>
+		</div>
+		<div id="box2" class="box">
+			<h1>กล่องที่ 2</h1>
+		</div>
+	</body>
+</html>

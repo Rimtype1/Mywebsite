5749574daa73b2cd8aa06f492c096e7ca861af93
Rimtype1 <100473568+Rimtype1@users.noreply.github.com>
2022-02-26 19:41:17 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -5,13 +5,11 @@
 			#box1{
 				width: 800px;
 				height: 350px;
+				background-color:lightblue
 			}
 			#box2{
 				width: 600px;
 				height: 150px;
-			}
-			.box{
-				background-color: green;
 			}
 			body{
 				background-color: grey;
@@ -19,11 +17,8 @@
 		</style>
 	</head>
 	<body>
-		<div id="box1" class="box">
+		<div id="กล่องที่1" class="box">
 			<h1>กล่องที่ 1</h1>
-		</div>
-		<div id="box2" class="box">
-			<h1>กล่องที่ 2</h1>
 		</div>
 	</body>
 </html>
